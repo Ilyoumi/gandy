cbd452ef9ee0c78b8fe193f9f7f324f0d7dc3068
yousraAb <yousraabid93@gmail.com>
2024-03-26 13:39:59 +0100

    create controller model api

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::middleware('auth:sanctum')->group(function () {
+    // Routes pour les utilisateurs
+    Route::apiResource('users', 'UserController');
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    // Routes pour les agendas
+    Route::apiResource('agendas', 'AgendaController');
+
+    // Routes pour les rendez-vous (RDVs)
+    Route::apiResource('rdvs', 'RdvController');
 });
